835452a73d7ce4861ab988b0a01dd17545417a20
Sherby-DevOps <pr.sherby@gmail.com>
2020-12-09 21:44:21 +0200

    Done !

--- server.js
+++ server.js
@@ -3,17 +3,49 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { Stats } = require('fs');
+const port = 8080;
 // Start up an instance of app
+const app = new express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // Cors for cross origin allowance
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Routes
+// Add the new data
+app.post('/add-data',add_data);
 
+function add_data(req,res){
+    projectData['temp'] = req.body.temp;
+    projectData['date'] = req.body.date;
+    projectData['feeling'] = req.body.feeling;
+    console.log(JSON.stringify(projectData));
+    res.send(['200 OK']);
+
+}
+
+// Get the saved data
+app.get('/get-data',get_data);
+
+function get_data(req,res){
+    res.send(JSON.stringify(projectData));
+}
+
+// // We will use it to clear our project data
+// app.get('/',function(){
+//     projectData = {};
+// });
 // Setup Server
+app.listen(port,function(){
+    console.log(`Server start listening on port ${port}`);
+});
--- website/app.js
+++ website/app.js
@@ -3,3 +3,83 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+let baseUri = "http://api.openweathermap.org/data/2.5/weather?";
+let zip = "zip=";
+let api_key = "&appid=1df432b7d28fba133d1b340e211a19ee";
+let zipCodeValue = '';
+let feeling = '';
+let fullApiUrl = '';
+let postPayload = {};
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click',generate_event);
+/* Function called by event listener */
+function generate_event()
+{
+    zipCodeValue = document.getElementById('zip').value;
+    feeling = document.getElementById('feelings').value;
+    fullApiUrl = baseUri+zip+zipCodeValue+api_key;
+     get_weather_from_api(fullApiUrl).then(function(data){
+         postPayload['temp'] = data.main.temp;
+         postPayload['date'] = newDate;
+         postPayload['feeling'] = feeling;
+         console.log(data);
+         add_weather_data("http://127.0.0.1:8080/add-data",postPayload).then(function(data){
+            console.log(data);
+            get_recent_weather_data("http://127.0.0.1:8080/get-data").then(function(data){
+                document.getElementById('date').innerText = "Date: " + data.date;
+                document.getElementById('temp').innerText = "Temp: " + data.temp;
+                document.getElementById('content').innerText = "Feeling: " + data.feeling;
+            });
+         });
+     });
+    // console.log(d);
+}
+/* Function to GET Web API Data*/
+let get_weather_from_api = async(url)=>{
+    let response = await fetch(url);
+    try{
+        let newData = await response.json();
+        console.log(newData);
+        return newData;
+    }
+    catch(error)
+    {
+        console.error("[!] Error :" + error);
+    }
+}
+/* Function to POST data */
+let add_weather_data = async(url,data)=>{
+    let response = await fetch(url,{
+        method: 'POST',
+        credentials:"same-origin",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data),
+    });
+    try{
+        let newData = await response.json();
+        console.log(newData);
+        return newData;
+    }
+    catch(error)
+    {
+        console.error("[!] Error :" + error);
+    }
+}
+
+
+/* Function to GET Project Data */
+let get_recent_weather_data =async(url)=>{
+    let response = await fetch(url);
+    try{
+        let newData = response.json();
+        return newData;
+    }
+    catch(error)
+    {
+        console.error("[!] Error :" + error);
+    }
+}
